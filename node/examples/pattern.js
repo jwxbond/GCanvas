6b4dfce4c9eb51da9ed84d2cf98c5980033ed282
jiangweixing <weixing.jwx@alibaba-inc.com>
2020-11-03 22:55:47 +0800

    feat： add pattern tc

--- node/examples/pattern.js
+++ node/examples/pattern.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const path = require('path')
-const out = fs.createWriteStream(path.join(__dirname, "..")+ '/imageone.png');
+const out = fs.createWriteStream(path.join(__dirname, "..")+ '/pattern.png');
 const { createCanvas, Image } = require('../export');
-const canvas = createCanvas(400, 400);
+const canvas = createCanvas(800, 400);
 const ctx = canvas.getContext('2d');
 const img = new Image()
 
 img.onload = () => {
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.drawImage(img, 0, 0,100,100);
+    var pattern1 = ctx.createPattern(img, 'repeat');
+    ctx.fillStyle = pattern1;
+    ctx.fillRect(0, 0, img.width*3, img.height*1);
 
     var stream = canvas.createPNGStream();
     stream.on('data', function (chunk) {
