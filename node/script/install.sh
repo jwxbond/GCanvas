208739ab8f2b58403b05c91e538fbd5729879432
jiangweixing <weixing.jwx@alibaba-inc.com>
2020-11-24 17:01:41 +0800

    Feat: update install.sh

--- node/script/install.sh
+++ node/script/install.sh
@@ -1,4 +1,31 @@
-echo "start  build @gcanvs/node"
-sudo npm install cmake-js -g
-mkdir -p ~/.gAssets/fonts/ &&  cp ./third_party/font/wqy-microhei.ttc ~/.gAssets/fonts/wqy-microhei.ttc &&  cp ./third_party/font/.fontcache  ~/.gAssets/fonts/.fontcache
-npm run compile
+platformSupport=0
+unamestr=`uname`
+
+if [[ "$unamestr" == 'Darwin' ]]; then
+    platform='mac osx'
+    platformSupport=1
+elif [[ "$unamestr" == 'Linux' ]]; then
+    platform='linux'
+    platformSupport=2
+    mkdir -p ~/.gAssets/fonts/ &&  cp ./third_party/font/wqy-microhei.ttc ~/.gAssets/fonts/wqy-microhei.ttc &&  cp ./third_party/font/.fontcache  ~/.gAssets/fonts/.fontcache
+fi
+
+if [ $platformSupport -eq 0 ]; then
+    echo "not support at $unamestr"
+    exit 0
+else
+    echo "start build at $platform"
+
+    if [ $platformSupport -eq 1 ]; then
+        # brew update
+        brew install cairo pango libpng jpeg-turbo curl-openssl
+    else 
+        # apt-get update
+        apt-get install -y cmake libglfw3-dev libgles2-mesa-dev libboost-all-dev xvfb wget curl nodejs
+        apt-get install -y libcurl4-openssl-dev libjpeg-dev libcairo2-dev libpango1.0-dev libglib2.0-dev
+    fi
+    npm install cmake-js
+    npm run compile
+fi
+
+
